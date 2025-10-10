2556ad56cd53aebf24cd40e862883e50af5bd622
KhanitthaLyn <khanitthamfu@gmail.com>
2025-10-10 14:37:27 +0700

    add Analytics

--- src/main/java/com/ecommerce/myecommerceproject/repositories/OrderRepository.java
+++ src/main/java/com/ecommerce/myecommerceproject/repositories/OrderRepository.java
@@ -2,8 +2,12 @@ package com.ecommerce.myecommerceproject.repositories;
 
 import com.ecommerce.myecommerceproject.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query("SELECT COALESCE(SUM(o.totalAmount), 0) FROM Order o")
+    Double getTotalRevenue();
 }
