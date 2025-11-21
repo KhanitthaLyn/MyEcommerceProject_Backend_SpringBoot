a5472731656bd48ea3386223f6bb1f352f11f943
KhanitthaLyn <145004913+KhanitthaLyn@users.noreply.github.com>
2025-11-21 10:35:19 +0700

    Format AppRole enum for consistency
    Signed-off-by: KhanitthaLyn <145004913+KhanitthaLyn@users.noreply.github.com>

--- src/main/java/com/ecommerce/myecommerceproject/model/AppRole.java
+++ src/main/java/com/ecommerce/myecommerceproject/model/AppRole.java
@@ -1,7 +1,7 @@
 package com.ecommerce.myecommerceproject.model;
 
 public enum AppRole {
-    ROLE_USER,   // Regular user with basic access (e.g., browsing and purchasing products)
-    ROLE_ADMIN,  // Administrator with full access to manage the application
-    ROLE_SELLER  // Seller who can add and manage products for sale
+    ROLE_USER,   
+    ROLE_ADMIN, 
+    ROLE_SELLER 
 }
